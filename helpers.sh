7c4d085c1f3a533a8dd68cc07dde24accdef3c71
Tyler Stachecki <tstache1@cs.binghamton.edu>
2013-09-01 13:14:34 -0400

    Add utility/script functions.

---
+++ helpers.sh
@@ -0,0 +1,21 @@
+# =============================================================================
+#  watchgit: Helper scripts.
+#
+#  This file is subject to the terms and conditions defined in
+#  file 'LICENSE', which is part of this source code package.
+#
+#  Source this from your shell's rc.
+# =============================================================================
+
+# Changes directory to repository location.
+# Reaps for directories using `watchgit list`.
+function snap() {
+  place=`watchgit list | sed -n "s/${1}: //p"`
+
+  if [ -z "${place}" ]; then
+    echo 'watchgit: Unknown repository.'
+  else
+    cd "${place}"
+  fi
+}
+
